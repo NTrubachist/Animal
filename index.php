4b9b7afe44dde684f298f58f1d720c1ae1d1d9d8
Godislavs <vladislavs.gordejevs@gmail.com>
2017-04-24 19:12:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="eng">
 <head>
     asdasd
+    asdasd
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href = "./libs/bootstrap/css/bootstrap.css" rel ="stylesheet">
